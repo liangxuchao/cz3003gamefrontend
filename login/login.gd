20580dc7e49eff80493542884065e8bf0e3cdcad
liangxuchao <liangxuchao@gmail.com>
2021-04-07 06:12:16 -0700

    xc commit login api

--- login/login.gd
+++ login/login.gd
@@ -15,20 +15,18 @@ func _on_submit_pressed():
 		alertLabel.text = "Please key in \n both username \n and password!"
 		alertpopup.popup()
 	else:
-		var query = "?username="+username.text+"&password=" +password.text;
+		var query = to_json({"username":username.text,"password": password.text});
 		print(query)
-		print(Global.APIrooturl +  "/login")
-		#var headers: PoolStringArray = ["Content-Length: " + str(query.length()), 
-		#"Content-Type : multipart/form-data; boundary=1", 
-		#"HOST: cz3003arsenal.southeastasia.cloudapp.azure.com:8080"] 
-		
+		print(Global.APIrooturl +  "/login/v2")
+		var headers = ["Content-Type: application/json","Content-Length: " + str(query.length())] 
+
 		httpNode.connect("request_completed", self, "_on_request_completed_login")
-		httpNode.request(Global.APIrooturl +  "/login" + query,[],false,HTTPClient.METHOD_POST)
+		httpNode.request(Global.APIrooturl +  "/login/v2",headers,false,HTTPClient.METHOD_POST,query)
 		
 		
 func _on_request_completed_login(result, response_code,headers, body):	
 	var json = JSON.parse(body.get_string_from_utf8())
-	
+	print(json.result)
 	if response_code == 200:
 		var time = OS.get_unix_time()
 		Global.updateConfig({"Username":username.text, "AccessToken":json.result["access_token"], "TokenExpire": json.result["expires_in"], "lastlogin":time })
