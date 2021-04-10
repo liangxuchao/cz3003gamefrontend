d3409389c3819795e510577d4418fb6e58724bf5
liz <lizliaolizliaolizliao@gmail.com>
2021-04-10 11:13:37 +0800

    zx commit

--- game/pve/World2/Winner.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-
-func _ready():
-	pass # Replace with function body.
-
-
-func _on_Next_Button_pressed():
-	get_tree().change_scene("res://game/gameselection/world2/world2Section.tscn")
