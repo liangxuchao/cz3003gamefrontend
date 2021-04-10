7df925d4217ce9f60c2d5029e567029f14834f53
Ruan Yang <ruanyang0805@gmail.com>
2021-04-10 18:00:46 +0800

    leaderboard integrate with API

--- game/leaderboard/Panel.gd
+++ game/leaderboard/Panel.gd
@@ -3,28 +3,55 @@ extends Panel
 onready var httpNode = $HTTPRequest
 
 func _ready():
+	var authheader: PoolStringArray = ['Authorization: Bearer ' + Global.AccessToken, 'Content-Type: application/json', ] 
 	httpNode.connect("request_completed", self, "_on_request_completed")
-	#httpNode.request("http://www.mocky.io/v2/5185415ba171ea3a00704eed")
-	httpNode.request("https://data.gov.sg/api/action/datastore_search?resource_id=b2871270-4eef-44a3-be98-908e2a73b19f")
+	httpNode.request(Global.APIrooturl +  "/api/v1/dashboard/leaderboard",authheader,false,HTTPClient.METHOD_GET)
+
 func _on_request_completed(result, response_code,headers, body):	
 	var json = JSON.parse(body.get_string_from_utf8())
 	var shift = 0.0
 	var i = 1
+	
+	$ScrollContainer/GridContainer.set_columns(2)
+	var dynamic_font = DynamicFont.new()
+	dynamic_font.font_data = load("res://public/fonts/soupofjustice.ttf")
+	dynamic_font.size = 30
+	
+	var headerName = Label.new()
+	headerName.set_align(Label.ALIGN_LEFT)
+	headerName.set_text("NAME")
+	headerName.add_color_override("font_color",Color(0.5,0.66,0.48))
+	headerName.set("custom_fonts/font",dynamic_font)
+	
+	var headerScore = Label.new()
+	headerScore.set_align(Label.ALIGN_LEFT)
+	headerScore.set_text("SCORE")
+	headerScore.size_flags_horizontal = Control.SIZE_SHRINK_END + Control.SIZE_EXPAND
+	headerScore.add_color_override("font_color",Color(0.5,0.66,0.48))
+	headerScore.set("custom_fonts/font",dynamic_font)
+
+	$ScrollContainer/GridContainer.add_child(headerName)
+	$ScrollContainer/GridContainer.add_child(headerScore)
 	if json != null:
-		for player in json.result["result"]["records"]:
+		for player in json.result:
 			i=i+1
 			if i == 50:
 				break
 			else:
 				var lbl = Label.new()
-				lbl.align = Label.ALIGN_CENTER
-				lbl.text = player.name
+				lbl.set_align(Label.ALIGN_LEFT)
+				lbl.set_text(player.name)
 				lbl.add_color_override("font_color",Color(0.5,0.66,0.48))
-				var dynamic_font = DynamicFont.new()
-				dynamic_font.font_data = load("res://public/fonts/soupofjustice.ttf")
-				dynamic_font.size = 20
 				lbl.set("custom_fonts/font",dynamic_font)
-				$ScrollContainer/VBoxContainer.add_child(lbl)
+				$ScrollContainer/GridContainer.add_child(lbl)
+				
+				var score = Label.new()
+				score.set_align(Label.ALIGN_LEFT)
+				score.set_text(str(player.score))
+				score.size_flags_horizontal = Control.SIZE_SHRINK_END + Control.SIZE_EXPAND
+				score.add_color_override("font_color",Color(0.5,0.66,0.48))
+				score.set("custom_fonts/font",dynamic_font)
+				$ScrollContainer/GridContainer.add_child(score)
 				
 
 				
