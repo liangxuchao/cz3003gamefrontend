c2e1409700a230575a9fd1e88df3764746b45072
liangxuchao <liangxuchao@gmail.com>
2021-04-25 06:55:51 -0700

    xc commit fix world2 bugs

--- game/gameselection/chooseSection/worldSection.gd
+++ game/gameselection/chooseSection/worldSection.gd
@@ -64,6 +64,7 @@ func _on_Close_pressed():
 	levelpopup.visible = false
 
 func _on_level_pressed(level):
+	#print(Global.pvesection.levels[1])
 	Global.pvelvl = Global.pvesection.levels[level]
 	get_tree().change_scene("res://game/pve/"+ Global.worldmapper[Global.pveworld.name] +"/PVECombat.tscn")
 	pass # Replace with function body.
